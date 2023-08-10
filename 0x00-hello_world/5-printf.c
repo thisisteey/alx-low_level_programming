728e60cd3ea1461e555cb60d94997ea2a8ec061d
thisisteey <dadataiwo82@yahoo.com>
2023-08-10 09:25:22 -0700

    c programm that uses the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point
+ *
+ * Return: 0 when code is successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
