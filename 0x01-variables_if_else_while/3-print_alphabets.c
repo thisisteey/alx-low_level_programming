7f282f5d359f11e2c235101feb241a283b9c2b66
thisisteey <dadataiwo82@yahoo.com>
2023-08-11 07:21:08 -0700

    C program to print the alphabets in lowercase and uppercase using only putchar three times

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the code
+ *
+ * Return: 0 on each code success
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
