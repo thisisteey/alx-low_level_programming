039bcd389e4d9beb475291e6d59652fe1a779914
thisisteey <dadataiwo82@yahoo.com>
2023-08-11 07:12:19 -0700

    C program to print out the alphabets in lowercase using putchar only twice

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the code
+ *
+ * Return: 0 on code success
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
