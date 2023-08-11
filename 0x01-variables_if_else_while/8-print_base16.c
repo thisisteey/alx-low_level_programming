77d172d3896ee207bed2fc6feefd305816c949d5
thisisteey <dadataiwo82@yahoo.com>
2023-08-11 08:58:47 -0700

    C program to print all numbers in base 6 and in lowercase using putchar only three times

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the code
+ *
+ * Return: 0 on success of the code
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
