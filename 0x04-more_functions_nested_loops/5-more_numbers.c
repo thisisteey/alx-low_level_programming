b534185e593c27e3d63ef92b74ffc13cd442a667
thisisteey <dadataiwo82@yahoo.com>
2023-08-17 06:38:52 -0700

    C program function that prints 10 times the numbers, from 0 to 14, followed by a new line, using _putchar only 3 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,10 @@ void more_numbers(void)
 	{
 		for (y = 0 ; y <= 14 ; y++)
 		{
+			if (y > 9)
+				_putchar(y / 10 + '0');
 			_putchar(y % 10 + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
