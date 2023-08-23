3e428a26b7b2963f2c4dd0517363d010c2afbb9f
thisisteey <dadataiwo82@yahoo.com>
2023-08-23 16:10:13 -0700

    C program that writes a function that prints a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -7,6 +7,7 @@
  * @size: size of buffer
  * Return: void
  */
+
 void print_buffer(char *b, int size)
 {
 	int o, j, i;
@@ -22,18 +23,18 @@ void print_buffer(char *b, int size)
 	{
 		j = size - o < 10 ? size - o : 10;
 		printf("%08x: ", o);
-		for (i = 0 ; i < 10 ; i++)
+		for (i = 0; i < 10; i++)
 		{
 			if (i < j)
 				printf("%02x", *(b + o + i));
 			else
-				printf(" ");
+				printf("  ");
 			if (i % 2)
 			{
 				printf(" ");
 			}
 		}
-		for (i = 0 ; i < j ; i++)
+		for (i = 0; i < j; i++)
 		{
 			int c = *(b + o + i);
 
