d30cbd2445639594f8fd8c76df975330fefc38c1
thisisteey <dadataiwo82@yahoo.com>
2023-08-30 09:41:53 -0700

    C program that writes a function that returns 1 if a string is a palindrome and 0 if not

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+
+int _strlen_recursion(char *s);
+int helper_palindrome(char *s, int len);
+
+/**
+ * _strlen_recursion - function to calculate the length of a string
+ * @s: string length to be calculated
+ * Return: length of string s
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (_strlen_recursion(s + 1) + 1);
+}
+
+/**
+ * is_palindrome - functions that checks for a plaindrome string
+ * @s: the string to be checked
+ * Return: 1 if the string is palindrome or 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = _strlen_recursion(s);
+	if (len <= 1)
+		return (1);
+	return (helper_palindrome(s, len));
+}
+
+/**
+ * helper_palindrome - function with a reversed string
+ * @s: input string
+ * @len: the length of string
+ * Return: a reverse string
+ */
+int helper_palindrome(char *s, int len)
+{
+	if (len <= 1)
+		return (1);
+	else if (*s == *(s + len - 1))
+	{
+		return (helper_palindrome(s + 1, len - 2));
+	}
+	else
+		return (0);
+}
