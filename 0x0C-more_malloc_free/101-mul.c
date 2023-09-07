200d58733111fd97101238328aafbf732b9e7a6a
thisisteey <dadataiwo82@yahoo.com>
2023-09-07 10:19:23 -0700

    C program that writes a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,24 +6,26 @@
 /**
  * _is_zero - determines if any number is zero
  * @argv: argument vector
- * Retun: void
+ * Return: no return
  */
 void _is_zero(char *argv[])
 {
 	int i, isn1 = 1, isn2 = 1;
 
-	for (i = 0 ; argv[1][i] ; i++)
+	for (i = 0; argv[1][i]; i++)
 		if (argv[1][i] != '0')
 		{
 			isn1 = 0;
 			break;
 		}
-	for (i = 0 ; argv[2][i] ; i++)
+
+	for (i = 0; argv[2][i]; i++)
 		if (argv[2][i] != '0')
 		{
 			isn2 = 0;
 			break;
 		}
+
 	if (isn1 == 1 || isn2 == 1)
 	{
 		printf("0\n");
@@ -32,7 +34,7 @@ void _is_zero(char *argv[])
 }
 
 /**
- * _initialize_array - set memory to zero in a new array
+ * _initialize_array - set memery to zero in a new array
  * @ar: char array
  * @lar: length of the char array
  * Return: pointer of a char array
@@ -41,36 +43,38 @@ char *_initialize_array(char *ar, int lar)
 {
 	int i = 0;
 
-	for (i = 0 ; i < lar ; i++)
+	for (i = 0; i < lar; i++)
 		ar[i] = '0';
 	ar[lar] = '\0';
 	return (ar);
 }
+
 /**
- * _checknum - determines lenght of the number
- * and checks if the number is in base 10
- * @argv: argument vector
+ * _checknum - determines length of the number
+ * and checks if number is in base 10
+ * @argv: arguments vector
  * @n: row of the array
- * Return: lenght of the number
+ * Return: length of the number
  */
 int _checknum(char *argv[], int n)
 {
 	int ln;
 
-	for (ln = 0; argv[n][ln] ; ln++)
+	for (ln = 0; argv[n][ln]; ln++)
 		if (!isdigit(argv[n][ln]))
 		{
 			printf("Error\n");
 			exit(98);
 		}
+
 	return (ln);
 }
 
 /**
- * main - entry point of program that multiplies two positive numbers
+ * main - Entry point program that multiplies two positive numbers
  * @argc: number of arguments
- * @argv: argument vector
- * Return: 0 on success
+ * @argv: arguments vector
+ * Return: 0 - success
  */
 int main(int argc, char *argv[])
 {
@@ -85,7 +89,7 @@ int main(int argc, char *argv[])
 		printf("Error\n"), exit(98);
 	nout = _initialize_array(nout, lnout);
 	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
-	for (; k >= 0 ; k--, i--)
+	for (; k >= 0; k--, i--)
 	{
 		if (i < 0)
 		{
@@ -104,7 +108,7 @@ int main(int argc, char *argv[])
 				break;
 			lnout--;
 			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
-			k = lnout - 1, i = ln1, j = ln2 - 1, ca = addl = 0;
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 		}
 		if (j >= 0)
 		{
