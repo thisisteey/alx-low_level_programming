77c4a7df16d8b947dccb59167095ec92aee8e389
thisisteey <dadataiwo82@yahoo.com>
2023-09-25 13:01:36 -0700

    C program that writes a function that prints all the elements of a listint_t list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,11 +9,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t numnodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		numnodes++;
 		printf("%d\n", h->n);
 		h = h->next;
+		numnodes++;
 	}
 	return (numnodes);
 }
