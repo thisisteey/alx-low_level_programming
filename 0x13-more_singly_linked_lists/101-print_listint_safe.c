caa8381ddfcb18967d83a1092fdf86415b1571e6
thisisteey <dadataiwo82@yahoo.com>
2023-09-25 16:05:21 -0700

    C program that writes a function that prints a listint_t linked list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a list safely
+ * @head: pointer to the head of a list
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t numnode = 0;
+	const listint_t *initialhead = NULL;
+
+	while (head != NULL && head != initialhead)
+	{
+		if (initialhead == NULL)
+			initialhead = head;
+		printf("[%p] %d\n", (void *) head, head->n);
+		head = head->next;
+		numnode++;
+	}
+	return (numnode);
+}
