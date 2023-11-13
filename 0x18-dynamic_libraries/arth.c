d823e2f7e900a3e8574ce7be9fd828afd71f91b1
thisisteey <dadataiwo82@yahoo.com>
2023-11-13 07:17:45 -0800

    C program that creates a dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/arth.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+
+/**
+ * add - gets the sum of two integers
+ * @x: first integer
+ * @y: second integer
+ * Return: the sum of the two integers
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
+
+/**
+ * sub - gets the difference of two integers
+ * @x: first integer
+ * @y: second integer
+ * Return: the difference of the two integers
+ */
+int sub(int x, int y)
+{
+	return (x - y);
+}
+
+/**
+ * mod - gets the remainder of two integers
+ * @x: first integer
+ * @y: second integer
+ * Return: the remainder of the two integers
+ */
+int mod(int x, int y)
+{
+	return (x % y);
+}
+
+/**
+ * mul - gets the multiplication of two integers
+ * @x: first integer
+ * @y: second integer
+ * Return: the multiplication of the two integers
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
+
+/**
+ * divid - gets the division of two integers
+ * @x: first integer
+ * @y: second integer
+ * Return: the division of the two integers
+ */
+int divid(int x, int y)
+{
+	return (x / y);
+}
