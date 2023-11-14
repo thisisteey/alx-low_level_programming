d4de9406b0ac7e613b301d7b5ddf8fd091bdd340
thisisteey <dadataiwo82@yahoo.com>
2023-11-13 16:15:21 -0800

    Code injection for giga millions projects

---
+++ 0x18-dynamic_libraries/gigtest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * printf - function to print formated output
+ * @format: string pointe containing characters
+ * @...: variable argument lists for more arguments
+ * Return: void
+ */
+int printf(const char *format, ...)
+{
+	printf("9 8 10 24 75 - 9\n");
+	puts("Congratulations, you win the Jackpot!");
+	exit(EXIT_SUCCESS);
+}
